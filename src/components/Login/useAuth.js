fcd256e862711d228f84e102d83bc7cafd35c61b
Arif <arif.swfu@outlook.com>
2020-03-21 14:08:58 +0800

    done

--- src/components/Login/useAuth.js
+++ src/components/Login/useAuth.js
@@ -1,21 +1,27 @@
-import React, { Children, useContext, useEffect } from 'react'
+// Hook (use-auth.js)
+import React, { useContext, useEffect, useState, createContext } from 'react'
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from "../../firebase.config";
-import { useState, createContext } from "react";
 import { Route, Redirect } from 'react-router-dom';
 
-
+// Add your Firebase credentials
 firebase.initializeApp(firebaseConfig)
 
 const AuthContext = createContext()
 
+// Provider component that wraps your app and makes auth object ...
+// ... available to any child component that calls useAuth().
 export const AuthContextProvider = (props) => {
     const auth = Auth()
     return <AuthContext.Provider value={auth}>{props.children}</AuthContext.Provider>
 }
 
+// Hook for child components to get the auth object ...
+// ... and re-render when it changes.
 export const useAuth = () => useContext(AuthContext)
+
+
 
 export const PrivateRoute = ({children, ...rest}) => {
     const auth = useAuth()
